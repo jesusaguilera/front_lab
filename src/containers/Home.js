a16b709c936825f73ddc20c9fbccb628593e0df8
jesusaguilera <jesusaguilera09@gmail.com>
2020-10-18 12:33:08 +0200

    removed split text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,16 +4,10 @@ import { useInView } from "react-intersection-observer";
 import { useSpring, a, config } from "react-spring";
 import { useDrag } from "react-use-gesture";
 
-import useSplitText from "../hooks/useSplitText";
-
 // Components
 import Main from "../components/Main";
 
 const Home = () => {
-
-  const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit faucibus diam laoreet dignissim. Mauris et mi in enim eleifend vehicula. Morbi ut felis mollis, cursus lacus eget, tempor quam. Sed dictum arcu vel sapien tristique, sollicitudin lobortis urna molestie. Sed convallis luctus dapibus. Duis ultrices sapien felis, vel placerat nulla tincidunt nec. Praesent urna est, varius in viverra sed, facilisis quis augue. Sed ut auctor mauris."
-
-  const splitText = useSplitText(loremIpsum, "c-text-split__span");
 
   const [contentRef, inView] = useInView({
     triggerOnce: true,
@@ -49,9 +43,6 @@ const Home = () => {
       </section>
       <section className="o-container">
         <h1>Section 2</h1>
-        <p className="c-text-split">
-        {splitText()}
-        </p>
       </section>
       <a.section
         className="o-container"
